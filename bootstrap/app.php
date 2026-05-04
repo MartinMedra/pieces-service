5087fd84b8071742fa7e22cdaabb4b838f3e8263
Martin Medrano <mmedranom@tecnocomfenalco.edu.co>
2026-05-03 22:46:18 -0500

    Añadiendo config/cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\AutenticarConJWT;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->api(prepend: [
+            HandleCors::class,
+        ]);
+
         $middleware->alias([
             'jwt.verificacion' => AutenticarConJWT::class,
         ]);
